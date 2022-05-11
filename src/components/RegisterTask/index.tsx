2f1a3bf38c0e5827c4a3b5b77df425fa7c1d3abd
Felipe Bastos <felipe.bastos@dt3.com.br>
2022-05-11 09:57:19 -0300

    adicionando o formulário de criação de tasks

--- src/components/RegisterTask/index.tsx
+++ src/components/RegisterTask/index.tsx
@@ -1,14 +1,36 @@
-import React from "react";
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useState } from "react";
 import { Button, Container, Form, Input } from "./styles";
+import { v4 as uuidv4 } from 'uuid';
 
 export function RegisterTask() {
+  const [tasks, setTasks] = useState([{}]);
+  const [task, setTask] = useState("");
+
+  const id = uuidv4();
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    const newTask = {
+      id: id,
+      task,
+      createdAt: new Date(),
+    };
+    setTasks([...tasks, newTask]);
+    setTask("");
+    console.log(tasks);
+  }
+
   return (
     <Container>
-      <Form action="" >
-          <Input type="text" placeholder="Enter your task" />
-        <Button>
-          Cadastrar
-        </Button>
+      <Form onSubmit={handleSubmit}>
+        <Input
+          type="text"
+          placeholder="Enter your task"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <Button>Cadastrar</Button>
       </Form>
     </Container>
   );
